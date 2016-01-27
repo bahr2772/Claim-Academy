fe141e0b52d7a5562717b08b957f3e17fd845d9a
Justin Bahr <justin@yknotstudios.com>
2016-01-26 20:43:32 -0600

    comming file

---
+++ testOverride.java
@@ -0,0 +1,10 @@
+package test;
+
+public class testOverride extends test{
+	 public static void main(String[] args){
+	
+}
+	public static int myMethod(String anyString, int i) throws NumberFormatException{
+		return 10;
+	}
+}
